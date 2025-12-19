4901a501acdc5272bf7578b0c08d2c642a7dbe6c
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2025-12-20 01:15:33 +1000

    Update rename-music.sh

--- rename-music.sh
+++ rename-music.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
+# Set ID3 tags for music files based on their filenames.
+# Filenames should be in the format: "01 Song Title.mp3"
 
 ARTIST="Space Dreamer"
 ALBUM=""
 
-while getopts "A:a:" opt; do
+help() {
+  echo "Usage: $0 [-h] [-A album] [-a artist] file1 [file2 ...]"
+  echo "  also: id3v2 [-A album] [-a artist] file1 [file2 ...]"
+  exit 0
+}
+
+if [[ $# -eq 0 ]]; then
+  help
+fi
+
+while getopts "A:a:h" opt; do
     case "$opt" in
         A) ALBUM="$OPTARG" ;;
         a) ARTIST="$OPTARG" ;;
+        h) help ;;
     esac
 done
 shift $((OPTIND - 1))
