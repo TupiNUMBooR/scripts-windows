2aaabdaaa23220776317f2c45b8b9e856220c2ed
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2026-01-30 11:31:59 +0700

    Fix image processing logic and improve output handling in AMV creation script

--- sd-create-amv-1.sh
+++ sd-create-amv-1.sh
@@ -11,23 +11,29 @@ fi
 image="$1"
 music="$2"
 bin="$(dirname "$0")"
-upscaled_image="${image%.*}-2x.jpeg"
-output="$3"
+image2x="${image%.*}-2x.jpeg"
+output="$3.mp4"
 
-"$GIGAPIXEL" \
-    --verbose \
+if [[ ! -f "$image2x" ]]; then
+  "$GIGAPIXEL" \
     --model cgi \
     --scale 2 \
     --input "$image" \
     --suffix "-2x" \
     -f jpeg \
     --output "$(dirname "$image")"
+  echo "Created 2x image: $image2x"
+else
+  echo "Using existing 2x image: $image2x"
+fi
 
-ffmpeg -i "$image" -i "$music" \
+ffmpeg -i "$image2x" -i "$music" \
+  -hide_banner -y \
   -map 0:v -map 1:a \
   -c:v libx264 -preset slow -crf 15 \
   -c:a copy \
   -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
   -movflags +faststart \
   -pix_fmt yuv420p \
-  "$output.mp4"
+  "$output"
+echo "Created AMV: $output"
