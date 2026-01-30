f91673157457fb5183e70f94431ed3da4674445b
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2026-01-30 20:03:24 +0700

    Enhance script to ensure SD_TMP_DIR is set and clean up old temporary files

--- sd-create-new-music.sh
+++ sd-create-new-music.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # shopt -s nullglob
-
-# shellcheck disable=SC1091
 . .env
 
 dir="$(date +%m%d-%H%M)"
 
+: "${SD_TMP_DIR:?SD_TMP_DIR is not set}"
+find "$SD_TMP_DIR" -type f -mtime +7 -delete
+
 cd /mnt/d/k/art/manual-youtube
+
 mv music/*.txt bonus/ || :
 cp -n music/*.mp3 bonus/ || :
 cp -n images/*.jpeg bonus/ || :
+
 sd-autocut-2.sh images music "../$dir"
+
 /mnt/c/Windows/explorer.exe "d:\\k\\art\\$dir"
+
