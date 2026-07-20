6e331ea66cb71de8ec949eb9ae0ccc0b82d8599c
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2026-07-20 23:34:36 +0300

    Add basher caller

---
+++ basher.ps1
@@ -0,0 +1,26 @@
+param(
+    [Alias('b')]
+    [switch]$Build
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$WorkDir = (Get-Location).Path
+$BasherDir = Join-Path $PSScriptRoot '..\basher'
+
+$ComposeArgs = @('compose', 'run', '--rm')
+
+if ($Build) {
+    $ComposeArgs += '--build'
+}
+
+$ComposeArgs += @('--volume', "${WorkDir}:/workspace", 'basher')
+
+Push-Location $BasherDir
+try {
+    docker @ComposeArgs
+}
+finally {
+    Pop-Location
+}
