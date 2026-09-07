698527ca8944f314ff051efbcb20fa59273faeec
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2026-09-07 19:35:47 +0300

    fix: download subtitles separately by language

--- yt-dlp-subs.sh
+++ yt-dlp-subs.sh
@@ -1,2 +1,3 @@
 # yt-dlp --all-subs --write-auto-sub --skip-download "$1"
-yt-dlp --sub-langs en,ru --write-subs --write-auto-subs --skip-download "$1"
+yt-dlp --sub-langs ru --write-subs --write-auto-subs --skip-download "$1"
+yt-dlp --sub-langs en --write-subs --write-auto-subs --skip-download "$1"
